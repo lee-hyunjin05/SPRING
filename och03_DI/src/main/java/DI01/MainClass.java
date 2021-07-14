4dd22bf9484fb4ec3237b634565e6b6661677728
tkghlwhfu <tkghlwhfu@naver.com>
2021-07-14 18:48:19 +0900

    2021-07-14

---
+++ och03_DI/src/main/java/DI01/MainClass.java
@@ -0,0 +1,33 @@
+package DI01;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+//		MyCalculator myCalculator = new MyCalculator();
+//	//	Calculator calculator = new Calculator();
+//	//	myCalculator.setCalculator(calculator);
+//		myCalculator.setCalculator(new Calculator());
+//		myCalculator.setFirstNum(50);
+//		myCalculator.setSecondNum(5);
+//		
+//		myCalculator.add();
+//		myCalculator.sub();
+//		myCalculator.mul();
+//		myCalculator.div();
+		
+//      DI 구현 
+		String configLocation = "classpath:applicationCTX.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation);
+		MyCalculator myCalculator = 
+			ctx.getBean("myCalculator", MyCalculator.class);
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mul();
+		myCalculator.div();
+		
+	}
+
+}
---
+++ och03_DI/src/main/java/DI03/MainClass.java
@@ -0,0 +1,21 @@
+package DI03;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+	public static void main(String[] args) {
+	
+		String configLocation = "classpath:applicationCTX3.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation);
+		StudentInfo studentInfo = ctx.getBean("studentInfo", StudentInfo.class);
+		studentInfo.getStudentInfo();
+		
+		Student student2 = ctx.getBean("student2", Student.class);
+		studentInfo.setStudent(student2);
+		studentInfo.getStudentInfo();
+		System.out.println("student2.getAge()-->"+student2.getAge());
+		System.out.println(student2.getAge()+5);
+		ctx.close();
+		}
+}
