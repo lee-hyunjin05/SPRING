28ee0fc0d131157987d40cc15f324d6f6a915375
lee-hyunjin05 <tkghlwhfu@naver.com>
2021-10-21 08:03:20 +0900

    2021.10.21

---
+++ och07_MVC152/src/main/java/com/oracle/mvc152/StudentController.java
@@ -0,0 +1,37 @@
+package com.oracle.mvc152;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+
+	private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
+	@RequestMapping("/studentForm")
+	public String studentForm() {
+		logger.info("Welcome studentForm start...");
+		return "createPage";
+	}
+
+	@RequestMapping("/student/create")
+	public String studentCreate(@ModelAttribute("student")	Student student,  BindingResult result) {
+		String page = "createDonePage";
+//	 	 BindingResult -> 유효성 검증결과를 담아두는 객체 	
+		StudentValidator validator = new StudentValidator();
+		validator.validate(student, result);
+		if(result.hasErrors()) {
+			page = "createPage";
+		}
+		logger.info("Welcome studentCreate page-> {}" , page);
+		return page;
+	}
+	
+	
+	
+	
+}
