6dc931812b0cfa761baae3e06381c207b91f63ac
lee-hyunjin05 <tkghlwhfu@naver.com>
2021-10-28 10:10:41 +0900

    2021.10.28

---
+++ oBootMybatis03/src/main/java/com/oracle/oBootMybatis03/controller/EmpRestController.java
@@ -0,0 +1,52 @@
+package com.oracle.oBootMybatis03.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oracle.oBootMybatis03.model.Dept;
+import com.oracle.oBootMybatis03.model.SampleVO;
+import com.oracle.oBootMybatis03.service.EmpService;
+// @Controller + @ResponseBody
+@RestController
+public class EmpRestController {
+	@Autowired
+    private EmpService es; 
+	
+	@RequestMapping("/hello")
+	public SampleVO hello() {
+		System.out.println("EmpRestController Start...");
+		SampleVO  vo = new SampleVO();
+		vo.setFirstName("hello");
+		vo.setLastName("연");
+	    vo.setMno(123);
+		return   vo;	
+	}
+	
+	@RequestMapping("/helloText")
+	public String  helloText() {
+		System.out.println("EmpRestController Start...");
+        String hello = "안녕";
+		return   hello;	
+	}
+
+	@RequestMapping("/sample/sendVO2")
+	public SampleVO sendVO2(int deptno) {
+		  System.out.println("@RestController deptno->"+deptno);
+		  SampleVO vo = new SampleVO();
+		  vo.setFirstName("길동");
+		  vo.setLastName("홍");
+		  vo.setMno(deptno);
+		  return vo;
+	}	
+
+	@RequestMapping("/sendVO3")
+	public List<Dept> sendVO3() {
+		System.out.println("@RestController sendVO3  START");
+		List<Dept> deptList = es.deptSelect();
+		return deptList;
+	}
+
+}
